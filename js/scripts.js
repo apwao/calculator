12c0747b1ab8bd8fca41cb5df8c3304207cce201
Sowayi <sapwao@gmail.com>
2019-02-12 03:06:50 -0500

    Add division function

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,21 @@ return number1 + number2;
 };
 let number1 = parseInt(prompt("Enter a number:"));
 let number2 = parseInt(prompt ("Enter another number"));
-alert(add (number1 + number2));
+let result = add(number1,number2);
+alert(result);
 //Subtraction function
 let subtract = function(number3, number4) {
 return number3 - number4;
 };
 let number3 = parseInt(prompt ("Enter a number:"));
-let number4 = parseInt(prompt ("Enter a number:"));
-alert(subtract(number3, number4));
+let number4 = parseInt(prompt ("Enter another number:"));
+let results = subtract(number3, number4);
+alert(results);
+//Division function
+let divide = function(number5, number7) {
+return number5/number7;
+};
+let number5 = parseInt(prompt("Enter a number:"));
+let number7 = parseInt(prompt("Enter another number:"));
+let resultd  = divide(number5,number7);
+alert(resultd);
