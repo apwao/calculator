f45e6acc706cf0a76d1028f37b2a3a0331ed36c4
Sowayi <sapwao@gmail.com>
2019-02-12 02:41:12 -0500

    Add subtract function

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ return number1 + number2;
 let number1 = parseInt(prompt("Enter a number:"));
 let number2 = parseInt(prompt ("Enter another number"));
 alert(add (number1 + number2));
+//Subtraction function
+let subtract = function(number3, number4) {
+return number3 - number4;
+};
+let number3 = parseInt(prompt ("Enter a number:"));
+let number4 = parseInt(prompt ("Enter a number:"));
+alert(subtract(number3, number4));
